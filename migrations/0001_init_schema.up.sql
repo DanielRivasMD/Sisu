947720c704a9c721d351b8a36de9bdcd14f57ca0
Daniel Rivas <danielrivasmd@gmail.com>
2025-08-28 11:18:51 +0200

    update sql schema

--- migrations/0001_init_schema.up.sql
+++ migrations/0001_init_schema.up.sql
@@ -5,9 +5,10 @@ PRAGMA foreign_keys = ON;
 CREATE TABLE IF NOT EXISTS tasks (
 	id integer PRIMARY KEY AUTOINCREMENT,
 	name text NOT NULL,
+	tag text,
 	description text,
-	date_target DATETIME,
-	date_start DATETIME DEFAULT CURRENT_TIMESTAMP,
+	date_target datetime,
+	date_start datetime DEFAULT CURRENT_TIMESTAMP,
 	archived boolean DEFAULT FALSE
 );
 
@@ -15,7 +16,7 @@ CREATE TABLE IF NOT EXISTS tasks (
 CREATE TABLE IF NOT EXISTS sessions (
 	id integer PRIMARY KEY AUTOINCREMENT,
 	task integer NOT NULL,
-	date date,
+	date date NOT NULL,
 	duration_mins integer,
 	score_feedback integer,
 	notes text,
@@ -26,7 +27,7 @@ CREATE TABLE IF NOT EXISTS sessions (
 CREATE TABLE IF NOT EXISTS milestones (
 	id integer PRIMARY KEY AUTOINCREMENT,
 	task integer NOT NULL,
-	type TEXT,
+	type text,
 	value integer,
 	achieved date,
 	message text,
@@ -43,17 +44,11 @@ CREATE TABLE IF NOT EXISTS reviews (
 );
 
 ----------------------------------------------------------------------------------------------------
-CREATE TABLE IF NOT EXISTS config (
-	key TEXT PRIMARY KEY,
-	value text
-);
-
-----------------------------------------------------------------------------------------------------
 CREATE TABLE coach (
 	id integer PRIMARY KEY AUTOINCREMENT,
-	trigger TEXT NOT NULL, -- e.g. 'milestone', 'streak'
+	trigger text NOT NULL,
 	content text NOT NULL,
-	date date NULL -- when it was last sent
+	date date NULL
 );
 
 ----------------------------------------------------------------------------------------------------
@@ -61,19 +56,6 @@ CREATE TABLE calendar (
 	id integer PRIMARY KEY AUTOINCREMENT,
 	date date NOT NULL,
 	note text NOT NULL
-);
-
-----------------------------------------------------------------------------------------------------
-CREATE TABLE tags (
-	id integer PRIMARY KEY AUTOINCREMENT,
-	name text UNIQUE NOT NULL
-);
-
-----------------------------------------------------------------------------------------------------
-CREATE TABLE task_tags (
-	task integer NOT NULL REFERENCES tasks (id),
-	tag integer NOT NULL REFERENCES tags (id),
-	PRIMARY KEY (task, tag)
 );
 
 ----------------------------------------------------------------------------------------------------
